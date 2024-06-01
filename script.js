f8c7a167baacc678c43c0de90eab0c06f0e37356
sulcovooo <123903370+sulcovooo@users.noreply.github.com>
2024-06-01 13:32:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+document.getElementById('steamLogin').addEventListener('click', function() {
+    window.location.href = `https://steamcommunity.com/openid/login?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.ns=http://specs.openid.net/auth/2.0&openid.realm=https://sulcovooo.github.io/rust/&openid.return_to=https://sulcovooo.github.io/rust/auth/&response_type=code&client_id=C7A50EEF043FC18D873E6C8AA7FAB8DE`;
+});
+
+
 function sendMessage() {
     var nickname = document.getElementById("nickname").value.trim() || "Anonymous";
     var message = document.getElementById("message-input").value.trim();
@@ -26,7 +31,3 @@ function sendMessage() {
         chatBox.scrollTop = chatBox.scrollHeight; // Scroll to the bottom
     }
 }
- 
-document.getElementById('steamLogin').addEventListener('click', function() {
-    window.location.href = `https://steamcommunity.com/openid/login?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select&openid.identity=http://specs.openid.net/auth/2.0/identifier_select&openid.mode=checkid_setup&openid.ns=http://specs.openid.net/auth/2.0&openid.realm=https://sulcovooo.github.io/rust/&openid.return_to=https://sulcovooo.github.io/rust/auth/&response_type=code&client_id=C7A50EEF043FC18D873E6C8AA7FAB8DE`;
-});
